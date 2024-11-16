17f3c74effe82e6358ff6ae7d3fb00acb20a4e55
Kairan Zhai <kairan95.zhai@gmail.com>
2024-11-16 15:04:36 -0500

    add portfolio data getter/setter

--- src/main/java/data_access/FileUserDataAccessObject.java
+++ src/main/java/data_access/FileUserDataAccessObject.java
@@ -8,10 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-import entity.Stock;
-import entity.StockFactory;
-import entity.User;
-import entity.UserFactory;
+import entity.*;
 import use_case.change_password.ChangePasswordUserDataAccessInterface;
 import use_case.login.LoginUserDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
@@ -24,15 +21,18 @@ import use_case.watchlist.WatchListModifyDataAccessInterface;
 public class FileUserDataAccessObject implements WatchListDataAccessInterface, WatchListModifyDataAccessInterface {
 
     private final ArrayList<String> watchList = new ArrayList<>();
-    private final ArrayList<Stock> portfolioList = new ArrayList<Stock>();
+    private final ArrayList<SimulatedHolding> portfolioList = new ArrayList<>();
 
     private final String mainFilePath = "src/main/java/data_access/";
     private final String watchListFilePath = "watchlist.txt";
+    private final String portfolioFilePath = "portfolio.txt";
 
     private final StockFactory stockFactory;
+    private final SimulatedHoldingFactory simulatedHoldingFactory;
 
-    public FileUserDataAccessObject(StockFactory stockFactory) throws IOException {
+    public FileUserDataAccessObject(StockFactory stockFactory, SimulatedHoldingFactory simulatedHoldingFactory) throws IOException {
         this.stockFactory = stockFactory;
+        this.simulatedHoldingFactory = simulatedHoldingFactory;
     }
 
     // Watch list related APIs
@@ -78,5 +78,57 @@ public class FileUserDataAccessObject implements WatchListDataAccessInterface, W
     public void removeFromWatchList(String symbol) {
         watchList.remove(symbol);
         saveWatchList();
+    }
+
+    // Portfolio list related APIs
+    public ArrayList<SimulatedHolding> getPortfolioList() {
+        // Using FileReader and BufferedReader to read the file
+        try (BufferedReader reader = new BufferedReader(new FileReader(mainFilePath + portfolioFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                final String[] data = line.split(",");
+                final String symbol = data[0];
+                final double price = Double.parseDouble(data[1]);
+                final int amount = Integer.parseInt(data[2]);
+                final SimulatedHolding simulatedHolding = simulatedHoldingFactory.create(symbol, price, amount);
+
+                portfolioList.add(simulatedHolding);
+            }
+
+            return portfolioList;
+        }
+        catch (IOException ex) {
+            // Normally, this means the file doesn't exist
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void savePortfolioList() {
+        final BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(mainFilePath + watchListFilePath));
+            for (SimulatedHolding simulatedHolding : portfolioList) {
+                writer.write(simulatedHolding.getSymbol() + ",");
+                writer.write(simulatedHolding.getPurchasePrice() + ",");
+                writer.write(simulatedHolding.getPurchaseAmount() + ",");
+                writer.newLine();
+            }
+
+            writer.close();
+        }
+        catch (IOException ex) {
+            // Normally, this means the file doesn't exist
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void addToPortfolioList(SimulatedHolding simulatedHolding) {
+        portfolioList.add(simulatedHolding);
+        savePortfolioList();
+    }
+
+    public void removeFromPortfolioList(SimulatedHolding simulatedHolding) {
+        portfolioList.remove(simulatedHolding);
+        savePortfolioList();
     }
 }
