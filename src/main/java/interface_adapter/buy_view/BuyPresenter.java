b8fc830f90ca8be4356f2fcf930799901b0af6e0
Allen Huang <allenhuang041203@gmail.com>
2024-11-23 16:36:32 -0500

    Buy: quantity and price.

--- src/main/java/interface_adapter/buy_view/BuyPresenter.java
+++ src/main/java/interface_adapter/buy_view/BuyPresenter.java
@@ -1,9 +1,13 @@
 package interface_adapter.buy_view;
 
+import entity.CommonSimulatedHoldingFactory;
+import entity.SimulatedHolding;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.change_password.IsLoggedIn;
 import interface_adapter.change_password.LoggedInState;
 import interface_adapter.home_view.HomeViewModel;
+import interface_adapter.portfolio.PortfolioState;
+import interface_adapter.portfolio.PortfolioViewModel;
 import use_case.buy.BuyOutputBoundary;
 import use_case.buy.BuyOutputData;
 import use_case.login.LoginOutputBoundary;
@@ -12,23 +16,21 @@ import view.BuyView;
 
 public class BuyPresenter implements BuyOutputBoundary {
 
-    private BuyViewModel buyViewModel;
+    private final BuyViewModel buyViewModel;
+    private final PortfolioViewModel portfolioViewModel;
     private final ViewManagerModel viewManagerModel;
     private final HomeViewModel homeViewModel;
 
-    public BuyPresenter(BuyViewModel buyViewModel, ViewManagerModel viewManagerModel, HomeViewModel homeViewModel) {
+    public BuyPresenter(BuyViewModel buyViewModel, PortfolioViewModel portfolioViewModel, ViewManagerModel viewManagerModel, HomeViewModel homeViewModel) {
         this.buyViewModel = buyViewModel;
+        this.portfolioViewModel = portfolioViewModel;
         this.viewManagerModel = viewManagerModel;
         this.homeViewModel = homeViewModel;
     }
 
     @Override
     public void prepareSuccessView(BuyOutputData response) {
-        // On success, switch to the home view.
-        // Store the stock information later.
-
-        this.viewManagerModel.setState(homeViewModel.getViewName());
-        this.viewManagerModel.firePropertyChanged();
+        portfolioViewModel.firePropertyChanged("getPortfolioList");
     }
 
     @Override
