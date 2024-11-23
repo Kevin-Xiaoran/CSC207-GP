c614c54095ea8adef7dd7612d4f10df3146e0090
Reiuy <52073159+Re1uy@users.noreply.github.com>
2024-11-22 21:11:02 -0500

    Revert "help"

--- src/main/java/entity/SimulatedHolding.java
+++ src/main/java/entity/SimulatedHolding.java
@@ -19,16 +19,4 @@ public interface SimulatedHolding {
      */
     int getPurchaseAmount();
 
-    /**
-     * Sets the new purchase price of the stock.
-     * @param purchasePrice the new purchase price of the stock.
-     */
-    void setPurchasePrice(double purchasePrice);
-
-    /**
-     * Sets the new purchase amount of the stock.
-     * @param purchaseAmount the new purchase amount of the stock.
-     */
-    void setPurchaseAmount(int purchaseAmount);
-
 }
