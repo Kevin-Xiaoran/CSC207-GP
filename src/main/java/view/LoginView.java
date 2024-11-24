692f47447c7033c11586d673459e9d09b46d9fa7
Allen Huang <allenhuang041203@gmail.com>
2024-11-24 16:15:17 -0500

    You cannot return to the home page without continuing to log in

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -6,6 +6,12 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
