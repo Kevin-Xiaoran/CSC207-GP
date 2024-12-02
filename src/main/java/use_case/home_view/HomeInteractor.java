87da04dc8abf4184f0bc114f096f38e7e70cdd0e
unknown <kairan95.zhai@gmail.com>
2024-12-02 15:39:17 -0500

    add debugMode variable for debuging purpose

--- src/main/java/use_case/home_view/HomeInteractor.java
+++ src/main/java/use_case/home_view/HomeInteractor.java
@@ -1,6 +1,7 @@
 package use_case.home_view;
 
 import entity.CommonStockFactory;
+import entity.DebugMode;
 import entity.Stock;
 import entity.StockFactory;
 import use_case.portfolio.PortfolioDataAccessInterface;
@@ -40,23 +41,26 @@ public class HomeInteractor implements HomeInputBoundary {
             stockSymbol = stockSymbol.toUpperCase();
         }
 
-//        // Using real data to search stock data based on symbol
-//        try {
-//            final Stock stock = homeDataAccessInterface.getStock(stockSymbol);
-//            final SearchOutputData searchOutputData = new SearchOutputData(stock, false);
-//            homePresenter.prepareSuccessView(searchOutputData);
-//        }
-//        catch (Exception err) {
-//            err.printStackTrace();
-//            homePresenter.prepareFailView("Failed to fetch stock data");
-//        }
+        if (DebugMode.debugMode) {
+            // Using fake data to search stock data based on symbol
+            final StockFactory stockFactory = new CommonStockFactory();
+            final Stock stock = stockFactory.create(stockSymbol, 128.2, 322.1, 100002322, 500.1, 100.23);
 
-        // Using fake data to search stock data based on symbol
-        final StockFactory stockFactory = new CommonStockFactory();
-        final Stock stock = stockFactory.create(stockSymbol, 128.2, 322.1, 100002322, 500.1, 100.23);
-
-        final SearchOutputData searchOutputData = new SearchOutputData(stock, false);
-        homePresenter.prepareSuccessView(searchOutputData);
+            final SearchOutputData searchOutputData = new SearchOutputData(stock, false);
+            homePresenter.prepareSuccessView(searchOutputData);
+        }
+        else {
+            // Using real data to search stock data based on symbol
+            try {
+                final Stock stock = homeDataAccessInterface.getStock(stockSymbol);
+                final SearchOutputData searchOutputData = new SearchOutputData(stock, false);
+                homePresenter.prepareSuccessView(searchOutputData);
+            }
+            catch (Exception err) {
+                err.printStackTrace();
+                homePresenter.prepareFailView("Failed to fetch stock data");
+            }
+        }
     }
 
     @Override
@@ -72,13 +76,17 @@ public class HomeInteractor implements HomeInputBoundary {
         final StockFactory stockFactory = new CommonStockFactory();
 
         for (String symbol : watchListData) {
-//            // Using real data
-//            final Stock stock = homeDataAccessInterface.getStock(symbol);
+            if (DebugMode.debugMode) {
+                // Using fake data
+                final Stock stock = stockFactory.create(symbol, 0.0, 0.0, 0, 0.0, 0.0);
+                watchList.add(stock);
+            }
+            else {
+                // Using real data
+                final Stock stock = homeDataAccessInterface.getStock(symbol);
+                watchList.add(stock);
+            }
 
-            // Using fake data
-            final Stock stock = stockFactory.create(symbol, 0.0, 0.0, 0, 0.0, 0.0);
-
-            watchList.add(stock);
         }
 
         homePresenter.switchToWatchList(watchList);
@@ -99,17 +107,20 @@ public class HomeInteractor implements HomeInputBoundary {
         final ArrayList<String> watchListData = watchListDataAccessInterface.getWatchList();
         final ArrayList<Stock> watchList = new ArrayList<>();
 
-//        // Using real data
-//        for (String symbol : watchListData) {
-//            final Stock stockData = homeDataAccessInterface.getStock(symbol);
-//            watchList.add(stockData);
-//        }
-
-        // Using fake data
-        final StockFactory stockFactory = new CommonStockFactory();
-        for (String symbol : watchListData) {
-            final Stock stock = stockFactory.create(symbol, 0, 0, 100002322, 500.1, 100.23);
-            watchList.add(stock);
+        if (DebugMode.debugMode) {
+            // Using fake data
+            final StockFactory stockFactory = new CommonStockFactory();
+            for (String symbol : watchListData) {
+                final Stock stock = stockFactory.create(symbol, 0, 0, 100002322, 500.1, 100.23);
+                watchList.add(stock);
+            }
+        }
+        else {
+            // Using real data
+            for (String symbol : watchListData) {
+                final Stock stockData = homeDataAccessInterface.getStock(symbol);
+                watchList.add(stockData);
+            }
         }
 
         homePresenter.getWatchListData(watchList);
