3f27e882a141e76bd76b66c35482f943c2caa627
eric <zifan.guo@mail.utoronto.ca>
2024-12-03 03:23:30 -0500

    some checkstyle fix of portfolio and stock

--- src/main/java/interface_adapter/stock_view/StockController.java
+++ src/main/java/interface_adapter/stock_view/StockController.java
@@ -3,6 +3,9 @@ package interface_adapter.stock_view;
 import entity.Stock;
 import use_case.stock.StockInputBoundary;
 
+/**
+ * Controller for the Stock Use Case.
+ */
 public class StockController {
 
     private final StockInputBoundary stockInteractor;
@@ -11,10 +14,19 @@ public class StockController {
         this.stockInteractor = stockInteractor;
     }
 
+    /**
+     * Executes the buy Stock.
+     *  @param stock the stock to buy
+     */
     public void buyStock(Stock stock) {
         stockInteractor.buyStock(stock);
     }
 
+    /**
+     * Verify watchlist.
+     *  @param stock the username to sign up
+     *  @param shouldModifyData the username to sign up
+     */
     public void toggleWatchlist(Stock stock, Boolean shouldModifyData) {
         stockInteractor.toggleWatchlist(stock, shouldModifyData);
     }
